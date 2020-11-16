31b44f0ff1399c87d855c15f760c478995c3cb68
lkaae2018 <webmaster@pensionmira.dk>
2020-11-16 13:10:56 +0100

    Update lyskryds.py
    Ny version

--- lyskryds.py
+++ lyskryds.py
@@ -46,46 +46,6 @@ def redred(x):# Udgangs punkt for lyskrydset
         sleep(2)
         return NS(x)
 
-def NS(x): #NS gul lyser sammen med rød
-    NSgul.on()
-    sleep(1)
-    NSred.off() #NS rød og NS gul slukker
-    NSgul.off()
-    return NSgreenst()
-    
-def NSgreenst():# Grænt lys tænder og slukker efte 5 sek.
-    NSgreen.on() 
-    sleep(5)
-    NSgreen.off()
-    return NSgulst()
-
-def NSgulst(): # Gul tænder og slukker efter 2 sek.
-    NSgul.on()
-    sleep(1)
-    NSgul.off()
-    x="NS"
-    return redred(x)
-
-def EV(x):#EV gul lyser sammen med rød
-    EVgul.on()
-    sleep(1)
-    EVred.off()
-    EVgul.off()
-    return EVgreenst()
-
-def EVgreenst():# Grænt lys tænder og slukker efte 5 sek.
-    EVgreen.on()
-    sleep(5)
-    EVgreen.off()
-    return EVgulst()
-    
-def EVgulst():# Gul tænder og slukker efter 2 sek.
-    EVgul.on()
-    sleep(1)
-    EVgul.off()
-    x="EV"
-    return redred(x)
-
 
 state=redred(x="EV")
 while state: state=redred(x="EV")
